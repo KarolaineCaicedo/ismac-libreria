87fbff35693c1afbb1be601cb197510bfbd45dd6
MATUTINO <MATUTINO@DESKTOP-IOQJGHO>
2024-05-20 11:28:21 -0500

    KMCv0001 - Este es mi primer cambio en el Git

---
+++ src/com/distribuida/entities/Factura.java
@@ -0,0 +1,62 @@
+package com.distribuida.entities;
+
+import java.util.Date;
+
+public class Factura {
+	private int idFactura;
+	private String numFactura;
+	private Date fecha;
+	private double totalNeto;
+	private double iva;
+	//private int idCliente;
+	
+	public Factura() {}
+	public Factura(int idFactura, String numFactura, Date fecha, double totalNeto, double iva)  {
+	
+		this.idFactura = idFactura;
+		this.numFactura = numFactura;
+		this.fecha = fecha;
+		this.totalNeto = totalNeto;
+		this.iva = iva;
+	
+	}
+	public int getIdFactura() {
+		return idFactura;
+	}
+	public void setIdFactura(int idFactura) {
+		this.idFactura = idFactura;
+	}
+	public String getNumFactura() {
+		return numFactura;
+	}
+	public void setNumFactura(String numFactura) {
+		this.numFactura = numFactura;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public double getTotalNeto() {
+		return totalNeto;
+	}
+	public void setTotalNeto(Float totalNeto) {
+		this.totalNeto = totalNeto;
+	}
+	public double getIva() {
+		return iva;
+	}
+	public void setIva(Float iva) {
+		this.iva = iva;
+	
+	}
+	@Override
+	public String toString() {
+		return "Factura [idFactura=" + idFactura + ", numFactura=" + numFactura + ", fecha=" + fecha + ", totalNeto="
+				+ totalNeto + ", iva=" + iva + "]";
+	}
+	
+	
+
+}
---
+++ src/com/distribuida/entities/FacturaDetalle.java
@@ -0,0 +1,57 @@
+package com.distribuida.entities;
+
+public class FacturaDetalle {
+	
+	private int idFacturaDetalle;
+	private int cantidad;
+	private double subtotal;
+	private int idFactura;
+	private int idLibro;
+	public FacturaDetalle(int idFacturaDetalle, int cantidad, Float subtotal, int idFactura, int idLibro) {
+		
+		this.idFacturaDetalle = idFacturaDetalle;
+		this.cantidad = cantidad;
+		this.subtotal = subtotal;
+		this.idFactura = idFactura;
+		this.idLibro = idLibro;
+	}
+	public int getIdFacturaDetalle() {
+		return idFacturaDetalle;
+	}
+	public void setIdFacturaDetalle(int idFacturaDetalle) {
+		this.idFacturaDetalle = idFacturaDetalle;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public double getSubtotal() {
+		return subtotal;
+	}
+	public void setSubtotal(Float subtotal) {
+		this.subtotal = subtotal;
+	}
+	public int getIdFactura() {
+		return idFactura;
+	}
+	public void setIdFactura(int idFactura) {
+		this.idFactura = idFactura;
+	}
+	public int getIdLibro() {
+		return idLibro;
+	}
+	public void setIdLibro(int idLibro) {
+		this.idLibro = idLibro;
+	}
+	@Override
+	public String toString() {
+		return "FacturaDetalle [idFacturaDetalle=" + idFacturaDetalle + ", cantidad=" + cantidad + ", subtotal="
+				+ subtotal + ", idFactura=" + idFactura + ", idLibro=" + idLibro + "]";
+	}
+	
+	
+	
+
+}
